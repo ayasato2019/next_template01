690f516f6d3cb709ff223eb08c225abeb631409b
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2023-01-30 14:48:32 +0900

    envファイルうまくいった

--- microcms-next-jamstack-blog/next.config.js
+++ microcms-next-jamstack-blog/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-reactStrictMode: true,
+  reactStrictMode: true,
 }
 
-module.exports = {
-env: {
-	TEST_VAR_FOR_BROWSER: process.env.API_KEY,
-},
-};
+module.exports = nextConfig
