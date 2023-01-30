38c594c4f16a0c7703d25ae93eca76519b700477
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2023-01-30 11:58:29 +0900

    retry build

--- microcms-next-jamstack-blog/next.config.js
+++ microcms-next-jamstack-blog/next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    customKey: 'yw8UJBIBn0qg2EMMW2HPPNFPHdleyXXAuhBE',
+  },
+}
+nextConfig
