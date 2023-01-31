ec725c6148352972a98e69ffca8d5d35e96089d0
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2023-01-31 12:10:44 +0900

    MUI install & scss parts made

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,9 @@ import Link from "next/link";
 import { client } from "../libs/client";
 
 export default function Home({ blog }) {
-  return (
-    <div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+return (
+	<div>
+		<h1>First Post</h1>
+	</div>
+);
 }
-
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
-
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
-};
