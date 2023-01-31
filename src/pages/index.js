a005476299a9f801d8b8c65bc7dc51e4f559968c
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2023-01-31 13:31:00 +0900

    remove index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,24 @@ import { client } from "../libs/client";
 export default function Home({ blog }) {
 return (
 	<div>
-		<h1>First Post</h1>
+	<ul>
+		{blog.map((blog) => (
+		<li key={blog.id}>
+			<Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+		</li>
+		))}
+	</ul>
 	</div>
 );
 }
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+const data = await client.get({ endpoint: "blog" });
+
+return {
+	props: {
+	blog: data.contents,
+	},
+};
+};
