e13222aaa90bf0e6440c1ce96b8c2d3e1f633de5
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2023-02-06 11:57:49 +0900

    ハンバーガーメニュー下地

--- src/comprtnents/Header.tsx
+++ src/comprtnents/Header.tsx
@@ -5,54 +5,56 @@ import styles from '../styles/Home.module.scss';
 import UserInfo from './UserInfo';
 import { css } from '@emotion/react'
 
+// const 
+
 export default function Header() {
 	return (
 		<header className={styles['l-header']}>
 			<UserInfo />
-			<div className={styles['p-headerLogoNav']}>
-				<a href="/" className={styles['c-headerlogo-link']}>
+			<div className={styles['l-header-logo-nav__wrap']}>
+				<a href="/" className={styles['p-headerlogo__link']}>
 					<p className={styles['c-logo-description']}>こだわり手作りパンのサブスク</p>
 					<p className={styles['c-logo']}>Make it!</p>
 				</a>
-				<nav className={styles['p-header-nav']}>
-					<button className={styles['c-menu-trigger']}>
+				<nav className={styles['l-header-nav__wrap']}>
+					<button className={styles['p-header-nav__trigger']}>
 						<span></span>
 						<span></span>
 						<span></span>
 					</button>
-					<ul className={styles['p-header-nav-contents']}>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/about" className={styles['c-header-nav-contents-item-link']}>
+					<ul className={styles['l-header-nav__contents']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/about" className={styles['c-header-nav__contents-item-link']}>
 								Make it!について
 								<span>About</span>
 							</Link>
 						</li>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/courses" className={styles['c-header-nav-contents-item-link']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/courses" className={styles['c-header-nav__contents-item-link']}>
 								コース紹介
 								<span>Courses</span>
 							</Link>
 						</li>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/product" className={styles['c-header-nav-contents-item-link']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/product" className={styles['c-header-nav__contents-item-link']}>
 								商品例
 								<span>Product</span>
 							</Link>
 						</li>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/blog" className={styles['c-header-nav-contents-item-link']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/blog" className={styles['c-header-nav__contents-item-link']}>
 								商品開発ブログ
 								<span>Blog</span>
 							</Link>
 						</li>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/faq" className={styles['c-header-nav-contents-item-link']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/faq" className={styles['c-header-nav__contents-item-link']}>
 								よくある質問
 								<span>FAQ</span>
 							</Link>
 						</li>
-						<li className={styles['c-header-nav-contents-item']}>
-							<Link href="/contact" className={styles['c-header-nav-contents-item-link']}>
+						<li className={styles['p-header-nav__contents-item']}>
+							<Link href="/contact" className={styles['c-header-nav__contents-item-link']}>
 								お問合せ
 								<span>Contact</span>
 							</Link>
